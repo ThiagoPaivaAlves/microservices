8e7ddf6e96a4e060a0219bb42f42b9127cd90f1d
thiago <thiago_paiva@outlook.com>
2025-01-09 14:19:15 -0500

    Initialized project

---
+++ microservices-demo/src/test/java/com/microservices_project/microservices_demo/MicroservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices_project.microservices_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
